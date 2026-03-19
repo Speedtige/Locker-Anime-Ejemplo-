b6f66eb796ff7a8bde23038cf9c4c9e5fae93a7c
Cristian Veliz <68717302+Speedtige@users.noreply.github.com>
2026-03-19 16:11:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,542 @@
+/*
+{
+title:"Capítulo 2",
+image:"https://picsum.photos/400/200?2",
+desc:"Descripción del Capítulo 2.",
+video:"https://mega.nz/file/PhsnUSRS#sU9CLWGtajr88exfcaNf_j6JXlxHHW1L0D-eFEOyoKA"
+},
+{
+title:"Capítulo 3",
+image:"https://picsum.photos/400/200?5",
+desc:"Descripción del Capítulo 3.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/f_auto,q_auto/v1773805056/Season_2_-Arise_from_the_Shadow_tdsj45.mp4"
+},
+
+video:"https://mega.nz/file/PhsnUSRS#sU9CLWGtajr88exfcaNf_j6JXlxHHW1L0D-eFEOyoKA"
+video:"https://mega.nz/embed/PhsnUSRS#sU9CLWGtajr88exfcaNf_j6JXlxHHW1L0D-eFEOyoKA"
+
+Ejemplo de modificación de URL´s para Youtube:
+video:"https://www.youtube.com/watch?v=Bg--vA_Yf8s&list=RDBg--vA_Yf8s&start_radio=1\
+video:"https://www.youtube.com/embed/Bg--vA_Yf8s"
+
+
+video:"https://drive.google.com/file/d/1QF4pnbJPVETo1nqVffNJL_D_EdAFU_Ug/view?usp=sharing"
+video:"https://drive.google.com/file/d/1QF4pnbJPVETo1nqVffNJL_D_EdAFU_Ug/preview"
+
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/f_auto,q_auto/v1773805056/Season_2_-Arise_from_the_Shadow_tdsj45.mp4"
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773805056/Season_2_-Arise_from_the_Shadow_tdsj45.mp4"
+
+image:"https://picsum.photos/400/200?1"
+image:"anime/claymore/1.jpg"
+*/
+
+const projects = {
+
+anime1:[
+{
+title:"01 Claymore",
+image:"anime/claymore/1.jpg",
+desc:"Descripción del Capítulo 01.",
+video:"https://drive.google.com/file/d/1QF4pnbJPVETo1nqVffNJL_D_EdAFU_Ug/view?usp=sharing"
+},
+{
+title:"02 Claymore",
+image:"anime/claymore/1.jpg",
+desc:"Descripción del Capítulo 02.",
+video:"https://drive.google.com/file/d/1bGo5wXAs5x14z0DODe1w41TJzDT2vTy8/view?usp=sharing"
+},
+{
+title:"03 Claymore",
+image:"anime/claymore/1.jpg",
+desc:"Descripción del Capítulo 03.",
+video:"https://drive.google.com/file/d/1t82HmlbsHfwrfhVqB6-sDfzl66LlTLLF/view?usp=sharing"
+},
+{
+title:"04 Claymore",
+image:"anime/claymore/1.jpg",
+desc:"Descripción del Capítulo 04.",
+video:"https://drive.google.com/file/d/1ClSKnzIZ8qTEFaqx6fccMnqeHPAh8a-C/view?usp=sharing"
+},
+{
+title:"05 Claymore",
+image:"anime/claymore/2.jpg",
+desc:"Descripción del Capítulo 05.",
+video:"https://drive.google.com/file/d/14yWU0UpCECc3znF2hk5DXmFA6629UnwN/view?usp=sharing"
+},
+{
+title:"06 Claymore",
+image:"anime/claymore/2.jpg",
+desc:"Descripción del Capítulo 06.",
+video:"https://drive.google.com/file/d/1sazuETcI-0a40X-UpOD2PJFLeLsG0B1b/view?usp=sharing"
+},
+{
+title:"07 Claymore",
+image:"anime/claymore/2.jpg",
+desc:"Descripción del Capítulo 07.",
+video:"https://drive.google.com/file/d/1Q038Oijsw4OHeNkvdlxwIPnVjBQgB8Mi/view?usp=sharing"
+},
+{
+title:"08 Claymore",
+image:"anime/claymore/2.jpg",
+desc:"Descripción del Capítulo 08.",
+video:"https://drive.google.com/file/d/16vyyKraQ9GnomYDIgHLJPCN37SMED7-P/view?usp=sharing"
+},
+{
+title:"09 Claymore",
+image:"anime/claymore/3.jpg",
+desc:"Descripción del Capítulo 09.",
+video:"https://drive.google.com/file/d/16C5qCtaXIwP9MoS8pr3LCnPUawfsCwJW/view?usp=sharing"
+},
+{
+title:"10 Claymore",
+image:"anime/claymore/3.jpg",
+desc:"Descripción del Capítulo 10.",
+video:"https://drive.google.com/file/d/1iZXkXsNP_PQS5TwOVCbHwGZzMoNvKRSg/view?usp=sharing"
+},
+{
+title:"11 Claymore",
+image:"anime/claymore/3.jpg",
+desc:"Descripción del Capítulo 11.",
+video:"https://drive.google.com/file/d/1Ij24K3CEC1edyBdZwhxum6DHBffXSi7B/view?usp=sharing"
+},
+{
+title:"12 Claymore",
+image:"anime/claymore/3.jpg",
+desc:"Descripción del Capítulo 12.",
+video:"https://drive.google.com/file/d/1KA9F4LeVVxS1TWthRZR7F3CcmcBfKjYG/view?usp=sharing"
+},
+{
+title:"13 Claymore",
+image:"anime/claymore/4.jpg",
+desc:"Descripción del Capítulo 13.",
+video:"https://drive.google.com/file/d/1i2c6xssWWCBxSC7vqKiHlQZuY9MdUMVB/view?usp=sharing"
+},
+{
+title:"14 Claymore",
+image:"anime/claymore/4.jpg",
+desc:"Descripción del Capítulo 14.",
+video:"https://drive.google.com/file/d/1ZGrN2Fn8T1XMPiPrucWRd9F1JgPEmneO/view?usp=sharing"
+},
+{
+title:"15 Claymore",
+image:"anime/claymore/4.jpg",
+desc:"Descripción del Capítulo 15.",
+video:"https://drive.google.com/file/d/1AFsaD7IUd_jbm0nja7lBmhnwuEqDXxsM/view?usp=sharing"
+},
+{
+title:"16 Claymore",
+image:"anime/claymore/4.jpg",
+desc:"Descripción del Capítulo 16.",
+video:"https://drive.google.com/file/d/1Gm1lwyKE3cfG_j5JBC3FmxR5lZyWFpDo/view?usp=sharing"
+},
+{
+title:"17 Claymore",
+image:"anime/claymore/5.jpg",
+desc:"Descripción del Capítulo 17.",
+video:"https://drive.google.com/file/d/1BlZd91mE4IL9dVzL5jXigLmOwcO1djZm/view?usp=sharing"
+},
+{
+title:"18 Claymore",
+image:"anime/claymore/5.jpg",
+desc:"Descripción del Capítulo 18.",
+video:"https://drive.google.com/file/d/1mw7OpxUX2eWOOCCSBPEjnDkveqReyQEj/view?usp=sharing"
+},
+{
+title:"19 Claymore",
+image:"anime/claymore/5.jpg",
+desc:"Descripción del Capítulo 19.",
+video:"https://drive.google.com/file/d/1FdodM5Oz9O7yLm4uiNgZ_BnE4jS20N72/view?usp=sharing"
+},
+{
+title:"20 Claymore",
+image:"anime/claymore/5.jpg",
+desc:"Descripción del Capítulo 20.",
+video:"https://drive.google.com/file/d/1unfs_DyYUgzAE5kH0h9WNERvqHwSwMuJ/view?usp=sharing"
+},
+{
+title:"21 Claymore",
+image:"anime/claymore/6.jpg",
+desc:"Descripción del Capítulo 21.",
+video:"https://drive.google.com/file/d/1dHSp5VMYtGSWl6c6z420PN0HoUHZewOO/view?usp=sharing"
+},
+{
+title:"22 Claymore",
+image:"anime/claymore/6.jpg",
+desc:"Descripción del Capítulo 22.",
+video:"https://drive.google.com/file/d/1dH4zLDXpTB2ZsrztcW-JQfv7jvV1qXjA/view?usp=sharing"
+},
+{
+title:"23 Claymore",
+image:"anime/claymore/6.jpg",
+desc:"Descripción del Capítulo 23.",
+video:"https://drive.google.com/file/d/1CTnug4UiUC4-L3IHufvL27i36PJqn8qw/view?usp=sharing"
+},
+{
+title:"24 Claymore",
+image:"anime/claymore/6.jpg",
+desc:"Descripción del Capítulo 24.",
+video:"https://drive.google.com/file/d/1QTJ_jvzd6TsmPbIC6P6Dtd2CUOLa8VjV/view?usp=sharing"
+},
+{
+title:"25 Claymore",
+image:"anime/claymore/7.jpg",
+desc:"Descripción del Capítulo 25.",
+video:"https://drive.google.com/file/d/1yT2yge6Lrqu94ApGT63AcGsV-kSDrC17/view?usp=sharing"
+},
+{
+title:"26 Claymore",
+image:"anime/claymore/7.jpg",
+desc:"Descripción del Capítulo 26.",
+video:"https://drive.google.com/file/d/1eTNvSsOiOycYQyyxbyODIa2jiv-ueV4Z/view?usp=sharing"
+}
+],
+
+anime2:[
+{
+title:"Demon Spirit Seed Manual-Capítulo 1",
+image:"anime/Demon_Spirit/1.jpg",
+desc:"Descripción del Capítulo 1.",
+video:"https://mega.nz/file/XhtDlTiD#IdjRWe0nN7plHHCIEGPQJuSWkR840RFVn2mGNFEBoq4"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 2",
+image:"anime/Demon_Spirit/1.jpg",
+desc:"Descripción del Capítulo 2.",
+video:"https://mega.nz/file/e48xlKpL#FmFSuZ2Fpt7e-P3c_lplRWAxDo8eqNEkpxXfGQ_NTJs"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 3",
+image:"anime/Demon_Spirit/1.jpg",
+desc:"Descripción del Capítulo 3.",
+video:"https://mega.nz/file/v00WGDgC#jOD7nQxWEPteaDpQw3ukfhQvRddqyyzh8k5fkNpDq7g"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 4",
+image:"anime/Demon_Spirit/1.jpg",
+desc:"Descripción del Capítulo 4.",
+video:"https://mega.nz/file/XstAlRAC#jmvqHhG6kRJaXerRSV5eoew7AFBMadkU2ypMBN9w2NQ"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 5",
+image:"anime/Demon_Spirit/2.jpg",
+desc:"Descripción del Capítulo 5.",
+video:"https://mega.nz/file/rokiCYob#OfWNeEgXvq7pnWTU3zClW3FSeMM1yBHb7nZYAJTuus0"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 6",
+image:"anime/Demon_Spirit/2.jpg",
+desc:"Descripción del Capítulo 6.",
+video:"https://mega.nz/file/jt1lFARK#1cPf6AhbilIukY1aQ-biub44nobrkC_FDubuX1e9tQw"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 7",
+image:"anime/Demon_Spirit/2.jpg",
+desc:"Descripción del Capítulo 7.",
+video:"https://mega.nz/file/u9F1TI5K#P9uUzrKX68bJ04RvARy2Gyla1vwN2x93KTQv_41EyH0"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 8",
+image:"anime/Demon_Spirit/2.jpg",
+desc:"Descripción del Capítulo 8.",
+video:"https://mega.nz/file/ClkBxYzJ#QCLw0J-8-ip66sUSS2Dc_Bkb_-al0OAYfqOQP51YWiQ"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 9",
+image:"anime/Demon_Spirit/3.jpg",
+desc:"Descripción del Capítulo 9.",
+video:"https://mega.nz/file/ClcQVYKC#zTITaiorbTBs6QjDyv5qcK_vcuIH11e4jAbsIWu3dAA"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 10",
+image:"anime/Demon_Spirit/3.jpg",
+desc:"Descripción del Capítulo 10.",
+video:"https://mega.nz/file/Xt0ihBxA#PFpASEuvj2UeacWMcYv8ySusMnB9a2qumSC3PDppwBo"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 11",
+image:"anime/Demon_Spirit/3.jpg",
+desc:"Descripción del Capítulo 11.",
+video:"https://mega.nz/file/P11XkBDY#60TXnYREiCnpQVtpOSmoW0dPuMglTZY7IUCqvzsXBeM"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 12",
+image:"anime/Demon_Spirit/3.jpg",
+desc:"Descripción del Capítulo 12.",
+video:"https://mega.nz/file/ikcHhaaA#mc4bdIfHDV6p59kp21PvK3TzUe7jNcjEOjfVsfgsOio"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 13",
+image:"anime/Demon_Spirit/4.jpg",
+desc:"Descripción del Capítulo 13.",
+video:"https://mega.nz/file/3skXyALA#tRW9m4f2tWIT-IJC0JhtIAnXMt4pKECr1Y8xKxse9CA"
+},
+{
+title:"Demon Spirit Seed Manual-Capítulo 14",
+image:"anime/Demon_Spirit/4.jpg",
+desc:"Descripción del Capítulo 14.",
+video:"https://mega.nz/file/us0jzI7S#O31MBryOGwm8sfYSwAwvVktWe9YSDhBO2C4bHAdM2k8"
+}
+],
+
+anime3:[
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 1",
+image:"anime/Mushoku/1.jpg",
+desc:"Descripción del Capítulo 1.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854560/01_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_i3i08e.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 2",
+image:"anime/Mushoku/1.jpg",
+desc:"Descripción del Capítulo 2.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854534/02_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_iron7y.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 3",
+image:"anime/Mushoku/1.jpg",
+desc:"Descripción del Capítulo 3.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854579/03_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_er5mve.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 4",
+image:"anime/Mushoku/1.jpg",
+desc:"Descripción del Capítulo 4.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854403/04_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_cmtihj.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 5",
+image:"anime/Mushoku/2.jpg",
+desc:"Descripción del Capítulo 5.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773856320/05_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_z77zep.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 6",
+image:"anime/Mushoku/2.jpg",
+desc:"Descripción del Capítulo 6.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854692/06_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_vwrb1w.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 7",
+image:"anime/Mushoku/2.jpg",
+desc:"Descripción del Capítulo 7.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854544/07_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_c1ityf.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 8",
+image:"anime/Mushoku/2.jpg",
+desc:"Descripción del Capítulo 8.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854527/08_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_dpypza.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 9",
+image:"anime/Mushoku/3.jpg",
+desc:"Descripción del Capítulo 9.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854597/09_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_z10tq4.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 10",
+image:"anime/Mushoku/3.jpg",
+desc:"Descripción del Capítulo 10.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854760/10_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_uywg3w.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 11",
+image:"anime/Mushoku/3.jpg",
+desc:"Descripción del Capítulo 11.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854749/11_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_i09hej.mp4"
+},
+{
+title:"➣Mushoku No Eiyuu; Betsu Ni Skill Nanka Iranakatta N Da Ga (Hero Without A Class; Who Even!)-Capítulo 12",
+image:"anime/Mushoku/3.jpg",
+desc:"Descripción del Capítulo 12.",
+video:"https://res.cloudinary.com/dm4ocbqkt/video/upload/v1773854657/12_Final_Mushoku_No_Eiyuu_Betsu_Ni_Skill_Nanka_Iranakatta_N_Da_Ga_Hero_Without_A_Class_Who_Even_jcwleo.mp4"
+}
+]
+
+};
+
+function renderProjects(category,id){
+
+const container=document.getElementById(id);
+container.innerHTML="";
+
+projects[category].forEach(p=>{
+
+const card=document.createElement("div");
+card.className="project-card";
+
+card.innerHTML=`
+<img src="${p.image}" data-video-src="${p.video}">
+<div class="project-title">${p.title}</div>
+<div class="project-desc">${p.desc}</div>
+<button class="play-button">Reproducir</button>
+`;
+
+container.appendChild(card);
+
+// eventos aquí mismo
+card.querySelector('.play-button').addEventListener('click', () => {
+    openVideoPlayer(p.video);
+});
+
+card.querySelector('img').addEventListener('click', () => {
+    openVideoPlayer(p.video);
+});
+
+});
+
+}
+
+renderProjects("anime1","anime1Projects");
+renderProjects("anime2","anime2Projects");
+renderProjects("anime3","anime3Projects");
+
+const tabs=document.querySelectorAll(".tab");
+const contents=document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab=>{
+
+tab.addEventListener("click",()=>{
+
+tabs.forEach(t=>t.classList.remove("active"));
+contents.forEach(c=>c.classList.remove("active"));
+
+tab.classList.add("active");
+
+document.getElementById(tab.dataset.tab).classList.add("active");
+
+});
+
+});
+
+function getEmbedURL(url){
+
+    // 🔵 GOOGLE DRIVE
+    if(url.includes("drive.google.com")){
+        const match = url.match(/\/d\/(.*?)\//);
+        if(match){
+            return {
+                type:"iframe",
+                src:`https://drive.google.com/file/d/${match[1]}/preview`
+            };
+        }
+    }
+
+    // 🔴 YOUTUBE
+    if(url.includes("youtube.com") || url.includes("youtu.be")){
+        const match = url.match(/(v=|youtu\.be\/)([^&]+)/);
+        if(match){
+            return {
+                type:"iframe",
+                src:`https://www.youtube.com/embed/${match[2]}`
+            };
+        }
+    }
+
+    // 🟢 MP4 DIRECTO
+    if(url.endsWith(".mp4")){
+        return {
+            type:"video",
+            src:url
+        };
+    }
+
+    // 🟣 MEGA (solo embed)
+    if(url.includes("mega.nz/embed")){
+        return {
+            type:"iframe",
+            src:url
+        };
+    }
+
+    // ❌ TODO LO DEMÁS
+    return null;
+}
+
+function openVideoPlayer(videoSrc){
+
+    if(videoSrc.includes("mega.nz") && !videoSrc.includes("embed")){
+        showMegaModal(videoSrc);
+        return;
+    }
+
+    // 🔥 ELIMINA VIDEO ANTERIOR
+    const oldPlayer = document.querySelector('.video-player');
+    if(oldPlayer) oldPlayer.remove();
+
+    const videoData = getEmbedURL(videoSrc);
+
+    if(!videoData){
+        alert("❌ Este servidor no es compatible");
+        return;
+    }
+
+    const videoPlayer = document.createElement('div');
+    videoPlayer.classList.add('video-player');
+
+    let content = "";
+
+    if(videoData.type === "video"){
+        content = `
+        <video controls autoplay preload="metadata" width="100%">
+            <source src="${videoData.src}" type="video/mp4">
+        </video>`;
+    }
+
+    if(videoData.type === "iframe"){
+        content = `
+        <iframe src="${videoData.src}" allow="autoplay" allowfullscreen></iframe>`;
+    }
+
+    videoPlayer.innerHTML = content;
+    document.body.appendChild(videoPlayer);
+}
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('video-player')) {
+        const video = event.target.querySelector('video');
+        if(video){
+            video.pause();
+            video.src = "";
+        }
+        event.target.remove();
+    }
+});
+
+// Cerrar con la tecla Escape
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        const videoPlayer = document.querySelector('.video-player');
+        if (videoPlayer) {
+            videoPlayer.remove();
+        }
+    }
+});
+
+function showMegaModal(url){
+
+    const modal = document.createElement('div');
+    modal.classList.add('mega-modal');
+
+    modal.innerHTML = `
+        <div class="mega-box">
+            <h2>⚠️ Servidor externo</h2>
+            <p>Este video se abrirá en una nueva pestaña</p>
+            <div class="mega-buttons">
+                <button id="openMega">🚀 Abrir en MEGA</button>
+                <button id="cancelMega">Cancelar</button>
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(modal);
+
+    document.getElementById('openMega').onclick = () => {
+        window.open(url, "_blank");
+        modal.remove();
+    };
+
+    document.getElementById('cancelMega').onclick = () => {
+        modal.remove();
+    };
+}
